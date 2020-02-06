584fd70a46f6017bf65a6f6bb5b2b30d937ba463
James Kerslake <james@myconid.co.uk>
2020-02-06 19:37:51 +0000

    interactor base class

---
+++ src/core/interactors/Interactor.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace DrlArchive\core\interactors;
+
+
+use DrlArchive\core\classes\Request;
+use DrlArchive\core\classes\Response;
+use DrlArchive\core\interfaces\boundaries\InteractorInterface;
+use DrlArchive\core\interfaces\boundaries\PresenterInterface;
+
+abstract class Interactor implements InteractorInterface
+{
+    /**
+     * @var Response|null
+     */
+    protected $response;
+    /**
+     * @var PresenterInterface|null
+     */
+    private $presenter;
+    /**
+     * @var Request|null
+     */
+    private $request;
+
+    /**
+     * @param PresenterInterface|null $presenter
+     */
+    public function setPresenter(?PresenterInterface $presenter): void
+    {
+        $this->presenter = $presenter;
+    }
+
+    /**
+     * @param Request|null $request
+     */
+    public function setRequest(?Request $request): void
+    {
+        $this->request = $request;
+    }
+
+    protected function sendResponse(): void
+    {
+        $this->presenter->send($this->response);
+    }
+
+}
