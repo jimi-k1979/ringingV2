9e1abd64199366af4aae5fe36951bf8ab0ee4fa1
James Kerslake <james@myconid.co.uk>
2020-02-09 20:21:01 +0000

    updates phpunit bootstrap file

--- test/phpunit_bootstrap.php
+++ test/phpunit_bootstrap.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 putenv('ENVIRONMENT=testing');
 
 
-$_SERVER['DOCUMENT_ROOT'] = realpath(__DIR__ . '/html');
+$_SERVER['DOCUMENT_ROOT'] = realpath(__DIR__ . '/../html');
 //require_once __DIR__ . '/../../Connections/class.connection.php';
 //require_once __DIR__ . '/../../Connections/class.connection.replica.php';
 
@@ -13,7 +13,7 @@ $_SERVER['DOCUMENT_ROOT'] = realpath(__DIR__ . '/html');
 //    require_once $file;
 //}
 
-foreach (glob(__DIR__ . '/src/**/**/*.php') as $file) {
+foreach (glob(__DIR__ . '/../src/**/**/*.php') as $file) {
     require_once $file;
 }
 
@@ -24,10 +24,10 @@ foreach (glob(__DIR__ . '/src/**/**/*.php') as $file) {
 /**
  * Add mocks
  */
-//foreach (glob(__DIR__ . '/test/mocks/traits/*.php') as $file) {
-//    require_once $file;
-//}
-foreach (glob(__DIR__ . '/test/mocks/*.php') as $file) {
+foreach (glob(__DIR__ . '/traits/*.php') as $file) {
+    require_once $file;
+}
+foreach (glob(__DIR__ . '/mocks/*.php') as $file) {
     require_once $file;
 }
 //foreach (glob(__DIR__ . '/mocks/external/*.php') as $file) {
