cd8a08158bbbffc2e19d0ddc6163defb8b8cc17d
James Kerslake <james@myconid.co.uk>
2020-03-01 21:56:02 +0000

    Complete use case

--- test/mocks/DrlEventDummy.php
+++ test/mocks/DrlEventDummy.php
@@ -6,17 +6,19 @@ namespace mocks;
 
 use DrlArchive\core\entities\DrlEventEntity;
 use DrlArchive\core\interfaces\repositories\DrlEventRepositoryInterface;
+use traits\CreateMockDrlEventTrait;
 
 class DrlEventDummy implements DrlEventRepositoryInterface
 {
+    use CreateMockDrlEventTrait;
 
     public function insertEvent(DrlEventEntity $entity): DrlEventEntity
     {
-        return new DrlEventEntity();
+        return $this->createMockDrlEvent();
     }
 
     public function selectCompetition(int $id): DrlEventEntity
     {
-        return new DrlEventEntity();
+        return $this->createMockDrlEvent();
     }
 }
--- test/traits/CreateMockDrlEventTrait.php
+++ test/traits/CreateMockDrlEventTrait.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 namespace traits;
 
 
-use DrlArchive\core\entities\DrlCompetitionEntity;
 use DrlArchive\core\entities\DrlEventEntity;
-use DrlArchive\core\entities\LocationEntity;
 
 trait CreateMockDrlEventTrait
 {
+    use CreateMockLocationTrait;
+    use CreateMockDrlCompetitionTrait;
 
     private function createMockDrlEvent(): DrlEventEntity
     {
         $entity = new DrlEventEntity();
         $entity->setId(1234);
-        $entity->setLocation(new LocationEntity());
-        $entity->setCompetition(new DrlCompetitionEntity());
+        $entity->setLocation($this->CreateMockLocation());
+        $entity->setCompetition($this->createMockDrlCompetition());
         $entity->setYear('1970');
 
         return $entity;
