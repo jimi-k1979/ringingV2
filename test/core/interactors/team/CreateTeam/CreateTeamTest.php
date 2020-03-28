1ec15483ce418144cbf9ebc6668cfbdf0ba42a93
James Kerslake <james@myconid.co.uk>
2020-03-28 21:39:07 +0000

    create Team permissions

--- test/core/interactors/team/CreateTeam/CreateTeamTest.php
+++ test/core/interactors/team/CreateTeam/CreateTeamTest.php
@@ -6,14 +6,19 @@ namespace core\interactors\team\CreateTeam;
 use DrlArchive\core\classes\Response;
 use DrlArchive\core\entities\DeaneryEntity;
 use DrlArchive\core\entities\TeamEntity;
+use DrlArchive\core\Exceptions\AccessDeniedException;
 use DrlArchive\core\interactors\Interactor;
 use DrlArchive\core\interactors\team\CreateTeam\CreateTeam;
 use DrlArchive\core\interactors\team\CreateTeam\CreateTeamRequest;
 use DrlArchive\core\interfaces\repositories\DeaneryRepositoryInterface;
 use mocks\DeaneryDummy;
 use mocks\DeanerySpy;
+use mocks\GuestUserDummy;
+use mocks\LoggedInUserDummy;
 use mocks\PreseenterDummy;
 use mocks\PresenterSpy;
+use mocks\SecurityRepositoryDummy;
+use mocks\SecurityRepositorySpy;
 use mocks\TeamDummy;
 use mocks\TeamSpy;
 use mocks\TransactionManagerDummy;
@@ -45,11 +50,41 @@ class CreateTeamTest extends TestCase
         $useCase = new CreateTeam();
         $useCase->setRequest($request);
         $useCase->setPresenter(new PreseenterDummy());
+        $useCase->setSecurityRepository(new SecurityRepositoryDummy());
+        $useCase->setUserRepository(new LoggedInUserDummy());
         $useCase->setTeamRepository(new TeamDummy());
         $useCase->setDeaneryRepository(new DeaneryDummy());
         $useCase->setTransactionManager(new TransactionManagerDummy());
 
         return $useCase;
+    }
+
+    public function testUserIsAuthorised(): void
+    {
+        $securitySpy = new SecurityRepositorySpy();
+
+        $useCase = $this->createUseCase();
+        $useCase->setSecurityRepository($securitySpy);
+        $useCase->execute();
+
+        $this->assertTrue(
+            $securitySpy->hasIsUserAuthorisedCalled()
+        );
+    }
+
+    public function testGuestUserIsNotAuthorised(): void
+    {
+        $securitySpy = new SecurityRepositorySpy();
+        $userSpy = new GuestUserDummy();
+
+        $this->expectException(
+            AccessDeniedException::class
+        );
+
+        $useCase = $this->createUseCase();
+        $useCase->setSecurityRepository($securitySpy);
+        $useCase->setUserRepository($userSpy);
+        $useCase->execute();
     }
 
     public function testTransactionHasStarted(): void
