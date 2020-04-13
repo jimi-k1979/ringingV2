09db7c8a55195af72e0d51b2777a2f519391d863
James Kerslake <james@myconid.co.uk>
2020-04-13 15:16:00 +0100

    improved template with navbar

--- html/index.php
+++ html/index.php
@@ -4,15 +4,8 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/init.php';
 
-use Twig\Loader\FilesystemLoader;
-
-
-$loader = new FilesystemLoader(__DIR__ . '/templates');
-$twig = new Twig\Environment(
-    $loader,
-    [
-    ]
-);
-
-$twig->display('index.twig');
-
+try {
+    $twig->display('index.twig');
+} catch (Throwable $e) {
+    include __DIR__ . '/templates/failed.html';
+}
--- html/init.php
+++ html/init.php
@@ -4,4 +4,14 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-//session_start();
+session_start();
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment(
+    $loader,
+    [
+    ]
+);
