4f55c0028747b9559b5690ae2e2832a467709b5f
James Kerslake <james@myconid.co.uk>
2021-01-23 16:08:03 +0000

    Doctrine transaction factory

---
+++ src/implementation/factories/managers/DoctrineTransactionManagerFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrlArchive\implementation\factories\managers;
+
+
+use DrlArchive\core\interfaces\factories\managers\TransactionManagerFactoryInterface;
+use DrlArchive\core\interfaces\repositories\TransactionManagerInterface;
+use DrlArchive\implementation\repositories\doctrine\DoctrineDatabase;
+use DrlArchive\implementation\repositories\doctrine\DoctrineTransactionManager;
+
+class DoctrineTransactionManagerFactory implements
+    TransactionManagerFactoryInterface
+{
+
+    public function create(): TransactionManagerInterface
+    {
+        return new DoctrineTransactionManager(
+            DoctrineDatabase::createConnection()
+        );
+    }
+}
