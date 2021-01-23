fce9f2fe2585f9be59fb1f6e658dfc7bb96bf3d7
James Kerslake <james@myconid.co.uk>
2021-01-23 15:51:56 +0000

    global constants file

---
+++ src/core/Constants.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrlArchive\core;
+
+
+class Constants
+{
+    public const MINIMUM_YEAR = 1925;
+
+    public const FIELD_MESSAGE = 'message';
+    public const FIELD_CODE = 'code';
+}
