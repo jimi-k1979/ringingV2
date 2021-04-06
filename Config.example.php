d86e44e1ec212f9a4b120064df6b1b8527097863
James Kerslake <james@myconid.co.uk>
2021-04-06 19:40:01 +0100

    move environmental config

---
+++ Config.example.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Class Config
+ * File that sets the environmental configuration
+ */
+class ConfigExample
+{
+    public const DB_HOST = 'databaseHost';
+    public const DB_SCHEMA = 'databaseSchema';
+    public const DB_USER = 'databaseUser';
+    public const DB_PASSWORD = 'databasePassword';
+
+    public const HOST_NAME = 'applicationHostServer';
+}
--- src/Settings.php
+++ src/Settings.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace DrlArchive;
 
 
+use Config;
+
 class Settings
 {
-    public const DB_HOST = 'database';
-    public const DB_SCHEMA = 'ringingV2';
-    public const DB_USER = 'ringer';
-    public const DB_PASSWORD = 'ringerPassword';
+    public const DB_HOST = Config::DB_HOST;
+    public const DB_SCHEMA = Config::DB_SCHEMA;
+    public const DB_USER = Config::DB_USER;
+    public const DB_PASSWORD = Config::DB_PASSWORD;
+
+    public const HOST_NAME = Config::HOST_NAME;
 
     // --Commented out by Inspection (17/02/2021 20:18):public const PUBLIC_FOLDER = __DIR__ . '/../html/';
 
