c8c82fe26b16bc68991977f00e479e1d4062c3a6
James Kerslake <james@myconid.co.uk>
2021-04-07 19:34:47 +0100

    moves config files

--- src/ConfigExample.php
+++ src/ConfigExample.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace DrlArchive;
+
 /**
  * Class Config
  * File that sets the environmental configuration
---
+++ src/Implementation.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrlArchive;
+
+
+class Implementation
+{
+
+
+    // --Commented out by Inspection (17/02/2021 20:18):public const PUBLIC_FOLDER = __DIR__ . '/../html/';
+
+}
