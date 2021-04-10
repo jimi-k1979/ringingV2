4580b8003dee3e9da8d8f0709304d104a1c4beed
James Kerslake <james@myconid.co.uk>
2021-04-10 15:52:00 +0100

    PHPMailer SMTP setup

--- src/ConfigExample.php
+++ src/ConfigExample.php
@@ -10,10 +10,18 @@ namespace DrlArchive;
  */
 class ConfigExample
 {
+    public const HOST_NAME = 'applicationHostServer';
+
     public const DB_HOST = 'databaseHost';
     public const DB_SCHEMA = 'databaseSchema';
     public const DB_USER = 'databaseUser';
     public const DB_PASSWORD = 'databasePassword';
 
-    public const HOST_NAME = 'applicationHostServer';
+    public const SMTP_HOST_SERVER = 'smtpServer';
+    public const SMTP_REQUIRES_AUTHENTICATION = true;
+    public const SMTP_ENCRYPTION_PROTOCOL = 'smtpEncryptionProtocol';
+    public const SMTP_USERNAME = 'smtpUser';
+    public const SMTP_PASSWORD = 'smtpPassword';
+    public const SMTP_PORT = 587;
+
 }
