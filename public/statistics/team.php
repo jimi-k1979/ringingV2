7514ec068bcc48a8f2392be10076f02aba84db11
James Kerslake <james@myconid.co.uk>
2021-12-22 14:37:27 +0000

    some basic changes

---
+++ public/statistics/team.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../init.php';
+
+use DrlArchive\core\classes\Response;
+use DrlArchive\core\interactors\pages\judgePage\JudgePageRequest;
+use DrlArchive\core\interactors\pages\judgePage\JudgePageResponse;
+use DrlArchive\Implementation;
+use DrlArchive\implementation\factories\interactors\pages\JudgePageFactory;
+use DrlArchive\implementation\presenters\AbstractTwigPagePresenter;
+
+
+$presenter = new class extends AbstractTwigPagePresenter {
+    public function send(?Response $response = null): void
+    {
+        parent::send($response);
+        $this->dataForTemplate[self::NAV][self::NAV_HIGHLIGHTED] =
+            Implementation::NAV_HIGHLIGHT_STATS;
+
+        if ($response->getStatus() === Response::STATUS_SUCCESS) {
+            $this->dataForTemplate[self::TEAM] =
+                $response->getData()[JudgePageResponse::DATA_JUDGE];
+            $this->dataForTemplate[self::EVENTS] =
+                $response->getData()[JudgePageResponse::DATA_EVENTS];
+            $this->dataForTemplate[self::STATS] =
+                $response->getData()[JudgePageResponse::DATA_STATS];
+            try {
+                $this->twig->display(
+                    'statistics/team.twig',
+                    $this->dataForTemplate
+                );
+            } catch (Throwable $e) {
+                echo $response->getMessage();
+                die();
+            }
+        } else {
+            echo $response->getMessage();
+        }
+    }
+};
+
+$request = new JudgePageRequest();
+$request->setJudgeId((int)$_GET['id']);
+
+$useCase = (new JudgePageFactory())->create(
+    $presenter,
+    $request
+);
+$useCase->execute();
